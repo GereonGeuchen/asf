d4d5fe46a65680d80bd9603f4056396fc45abfbc
Hadar Shavit <shavit@aim.rwth-aachen.de>
2024-12-25 21:42:02 +0100

    Bug fix

--- asf/selectors/feature_generator.py
+++ asf/selectors/feature_generator.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 class AbstractFeatureGenerator:
-    def __init__(self, metadata: pd.DataFrame):
-        self.metadata = metadata
+    def __init__(self):
+        pass
 
     def generate_features(self, base_features) -> pd.DataFrame:
         pass
