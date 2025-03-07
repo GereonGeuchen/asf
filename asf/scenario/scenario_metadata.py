ad0ca5370e08cfe154fc4285469b5e6ca4e124fd
Hadar Shavit <shavit@aim.rwth-aachen.de>
2025-03-07 23:17:27 +0000

    Fix test

--- asf/scenario/scenario_metadata.py
+++ asf/scenario/scenario_metadata.py
@@ -13,15 +13,19 @@ class SelectionScenarioMetadata:
 
     def to_dict(self):
         """Converts the metadata into a dictionary format."""
-        return {
+        ret_dict =  {
             "algorithms": self.algorithms,
             "features": self.features,
             "performance_metric": self.performance_metric,
             "feature_groups": self.feature_groups,
             "maximize": self.maximize,
             "budget": self.budget,
-            "algorithm_features": self.algorithm_features,
         }
+
+        if self.algorithm_features is not None:
+            ret_dict["algorithm_features"] = self.algorithm_features
+        
+        return ret_dict
 
 
 @dataclass
