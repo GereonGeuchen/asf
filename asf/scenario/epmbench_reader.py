f8e053db5bb47f4284192bd328d92d9a5db7a696
Hadar Shavit <shavit@aim.rwth-aachen.de>
2025-04-07 23:18:44 +0000

    Rename EPM

--- asf/scenario/epmbench_reader.py
+++ asf/scenario/epmbench_reader.py
@@ -21,15 +21,15 @@ def read_epmbench_scenario(path):
 
     data = pd.read_parquet(os.path.join(path, "data.parquet"))
     if "groups" in metadata:
-        instances = data[metadata["groups"]]
+        groups = data[metadata["groups"]]
         data.drop(columns=[metadata["groups"]], inplace=True)
     else:
-        instances = None
+        groups = None
 
-    return data, metadata["features"], metadata["targets"], instances, metadata
+    return data, metadata["features"], metadata["targets"], groups, metadata
 
 
-def get_cv_fold(data, fold, features, target, instances=None):
+def get_cv_fold(data, fold, features, target, groups=None):
     """
     Splits the data into training and testing sets based on the specified fold.
 
@@ -53,11 +53,11 @@ def get_cv_fold(data, fold, features, target, instances=None):
     X_test = test_data[features]
     y_test = test_data[target]
 
-    if instances is not None:
-        instances_train = instances[train_idx]
-        instances_test = instances[test_idx]
+    if groups is not None:
+        groups_train = groups[train_idx]
+        groups_test = groups[test_idx]
     else:
-        instances_train = None
-        instances_test = None
+        groups_train = None
+        groups_test = None
 
-    return X_train, y_train, X_test, y_test, instances_train, instances_test
+    return X_train, y_train, X_test, y_test, groups_train, groups_test
