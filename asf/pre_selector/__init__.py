231d7cc2cb49877c423775c68923df6316c8f7f9
Hadar Shavit <shavit@aim.rwth-aachen.de>
2025-05-24 09:58:10 +0200

    Add brute force pre selectorto init

--- asf/pre_selector/__init__.py
+++ asf/pre_selector/__init__.py
@@ -3,10 +3,12 @@ from asf.pre_selector.marginal_contribution_based import (
 )
 from asf.pre_selector.optimize_pre_selection import OptimizePreSelection
 from asf.pre_selector.sbs_pre_selection import SBSPreSelector
+from asf.pre_selector.brute_force_pre_selection import BruteForcePreSelector
 
 
 __all__ = [
     "MarginalContributionBasedPreSelector",
     "OptimizePreSelection",
     "SBSPreSelector",
+    "BruteForcePreSelector"
 ]
