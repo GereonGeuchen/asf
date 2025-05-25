bc38bbb25a0236880116449ec1b22d39765b24e3
Hadar Shavit <shavit@aim.rwth-aachen.de>
2025-05-25 23:20:42 +0200

    knee of curve

--- asf/pre_selector/__init__.py
+++ asf/pre_selector/__init__.py
@@ -5,6 +5,7 @@ from asf.pre_selector.optimize_pre_selection import OptimizePreSelection
 from asf.pre_selector.sbs_pre_selection import SBSPreSelector
 from asf.pre_selector.brute_force_pre_selection import BruteForcePreSelector
 from asf.pre_selector.beam_search_pre_selection import BeamSearchPreSelector
+from asf.pre_selector.knee_of_the_curve_pre_selector import KneeOfCurvePreSelector
 
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     "SBSPreSelector",
     "BruteForcePreSelector",
     "BeamSearchPreSelector",
+    "KneeOfCurvePreSelector",
 ]
